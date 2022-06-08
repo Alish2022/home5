805cf68142a446bdc10fc62377c2e59460eedaf9
Alish2022 <a.rzaev2016@yandex.ru>
2022-06-08 16:26:56 +0300

    123

---
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import s from './App.module.css'
+import HW1 from '../../../p2-homeworks/h1/HW1'
+import HW5 from "../../../p2-homeworks/h5/HW5";
+import HW12 from "../../../p2-homeworks/h12/HW12";
+
+function App() {
+    return (
+        <div className={s.App}>
+            <div>react homeworks:</div>
+            <HW5/>
+        </div>
+    )
+}
+
+export default App
